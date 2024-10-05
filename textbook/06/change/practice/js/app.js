4171d7fa46658e772627f40b469a2386b17902f6
miyaguni-rikuto <120348141+miyaguni-rikuto@users.noreply.github.com>
2024-10-06 05:31:06 +0900

    a

--- textbook/06/change/practice/js/app.js
+++ textbook/06/change/practice/js/app.js
@@ -1,65 +1,21 @@
-  /************************************
- ジャンケンゲームを作る（関数なし）
-*************************************/
-//＜ジャンケンの手の番号を設定＞
-function janken(){
-    //＜「const」で変数宣言することで、define的な定数の使い方をする＞
-    const GU=1;
-    const CHOKI=2;
-    const PA=3;
-    //GUという変数を宣言、1を格納する
-    //CHOKIという変数を宣言、2を格納する
-    //PAという変数を宣言、3を格納する
+let practice = document.getElementById('practice');
+practice.innerHTML = '<h1>れんしゅう</h1>';
+practice.style.backgroundColor = '#999999';
+practice.style.fontSize = '30px';
+practice.style.color = '#FFFFFF';
 
-    //＜ジャンケンの入力ダイアログボックスを表示＞
-    function getHumHand(){
-        let hum = prompt('半角数字で1～3の数字を入力してください。\n\n' + GU + ':グー\n' + CHOKI + ':チョキ\n' + PA + ':パー'); //1:グー、2:チョキ、3:パーと、対応する変数と組み合わせて表示する。\nは改行コードなので、文字として扱う
-        hum = parseInt(hum,10);  
-        //<入力値のチェック>
-        if(hum != GU && hum != CHOKI && hum != PA){
-            return undefined //アラートで再読み込みの文章を表示
-        }else{
-            return hum;
-        }
-    }
-    
-    //<コンピューターの手を決める>
-    function getComHand(){
-    return Math.floor(Math.random()*3)+1;
-    }   
-        
+//要素を追加します。
+let first = document.createElement('div');
+first.setAttribute('id', 'first');
+first.innerHTML = '<p>要素を追加</p>';
+practice.insertBefore(first, null);
 
-    //＜コンピュータの手の名前＞
-    function  getHandName(num){
-    switch(num){
-        case GU:
-            return'グー';
-            break;
-        case CHOKI:
-            return'チョキ';
-            break;
-        case PA:
-            return'パー';
-            break;
+//さらに要素を追加します
+let second = document.createElement('div');
+second.setAttribute('id', 'second');
+second.innerHTML = '<p>さらに要素を追加</p>';
+practice.insertBefore(second, first);
 
-        }
-    }
-
-    alert(getResultMsg(com,hum));
-    return getResult(com,hum);
-    let win=0;
-    let isLose=false;
-    while(!isLose){
-        let result=janken();
-        if(result==='結果はあいこでした'){
-            continue;
-        }
-        if(result==='勝ちました'){
-            win++;
-        }
-       alert('ただいま「'+win+'」勝です。');
-       continue;
-    }
-    alert('連勝はストップです。記録は「'+win+'」勝でした');
-    isLose=true;
-}
+//要素を削除します
+let parent = first.parentElement;
+parent.removeChild(first);
