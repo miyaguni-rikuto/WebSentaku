862fd17643a217da372d355041f60c202b320dd0
miyaguni-rikuto <120348141+miyaguni-rikuto@users.noreply.github.com>
2025-03-12 16:43:08 +0900

    前期追試 7/25

--- textbook/08/array/practice/js/app.js
+++ textbook/08/array/practice/js/app.js
@@ -1,22 +1,5 @@
-//要素オブジェクトの取得
-let getResult= document.getElementById('getResult');
-let result=document.getElementById('result');
-
-//イベントの登録
-getResult.addEventListener('click',function(){
-result.innerHTML='結果は「' + omikuji.getResult() + '」でした';
-});
-
-//おみくじの結果データ作成
-let omikuji={
-    results: ['大吉','吉','中吉','小吉','凶'],
-    getResult:function(){
-        let results=this.results;
-        return results[Math.floor(Math.random() * results.length)];
-    }
-}
-
-console.log(omikuji.getResult());
+//おみくじの結果データを作成
+results = ['大吉', '吉', '中吉', '小吉', '凶'];
 
 //配列[results]をコンソールに表示
 console.log(results);
@@ -24,10 +7,9 @@ console.log(results);
 //インデックスが[0]の要素をコンソールに表示
 console.log(results[0]);
 
-
-//配列に所属するデータをfor文ですべて表示
-for (let i=0; i< result.length; i++){
-    console.log('index:'+i+'データ'+results[i]);
+//配列に属するデータをfor文ですべて表示
+for (let i = 0; i < results.length; i++) {
+    console.log('index:' + i + 'データ' + results[i]);
 }
 
 
